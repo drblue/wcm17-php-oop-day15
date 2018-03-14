aab251f741642b40fe6682092ea6e47de5202a2a
Johan Nordström <johan@digitalvillage.se>
2018-03-14 10:36:23 +0100

    Laravel 105 - Authentication.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Welcome to our Members Site!</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    You are {{ Auth::user()->name }}, duh!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/secret.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">This is top secret stuff!</div>
+
+                <div class="card-body">
+                    Much secret! Such wow! Very secure!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
